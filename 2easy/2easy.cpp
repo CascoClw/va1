ab3c1aa9d2774c2f91b8514ec87d8a7fd855c0e6
Michail_Daniil <casco.clw@mail.ru>
2024-07-01 01:02:05 +0300

    add file's easy

---
+++ 2easy/2easy.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+bool canFormWord(const string& word, const unordered_map<char, int>& charsCount) {
+    unordered_map<char, int> wordCount;
+    // Подсчитываем количество каждой буквы в слове
+    for (char c : word) {
+        wordCount[c]++;
+        // Если в слове больше символов, чем в доступных символах, возвращаем false
+        if (wordCount[c] > charsCount.at(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int countCharacters(vector<string>& words, string chars) {
+    unordered_map<char, int> charsCount;
+    // Подсчитываем количество каждой буквы в строке chars
+    for (char c : chars) {
+        charsCount[c]++;
+    }
+
+    int totalLength = 0;
+    // Проверяем каждое слово в words
+    for (const string& word : words) {
+        bool canForm = true;
+        unordered_map<char, int> wordCount;
+        for (char c : word) {
+            wordCount[c]++;
+            if (wordCount[c] > charsCount[c]) {
+                canForm = false;
+                break;
+            }
+        }
+        // Если слово можно сформировать, добавляем его длину к общему счетчику
+        if (canForm) {
+            totalLength += word.length();
+        }
+    }
+
+    return totalLength;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Примеры из задания
+    vector<string> words1 = { "кош", "bt", "шляпа", "дерево" };
+    string chars1 = "атач";
+    cout << "Input: words = {\"кош\", \"bt\", \"шляпа\", \"дерево\"}, chars = \"атач\", Output: " << countCharacters(words1, chars1) << endl; // Ожидаемый вывод: 6
+
+    vector<string> words2 = { "hello", "world", "leetcode" };
+    string chars2 = "welldonehoneyr";
+    cout << "Input: words = {\"hello\", \"world\", \"leetcode\"}, chars = \"welldonehoneyr\", Output: " << countCharacters(words2, chars2) << endl; // Ожидаемый вывод: 10
+
+    return 0;
+}
---
+++ 3easy/3easy.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string largestOddNumber(string num) {
+    // Проходим строку с конца, чтобы найти первую нечетную цифру
+    for (int i = num.size() - 1; i >= 0; --i) {
+        // Проверяем, является ли текущая цифра нечетной
+        if ((num[i] - '0') % 2 != 0) {
+            // Возвращаем подстроку от начала до текущей позиции включительно
+            return num.substr(0, i + 1);
+        }
+    }
+    // Если не найдено нечетных цифр, возвращаем пустую строку
+    return "";
+}
+
+int main() {
+    // Примеры из задания
+    string num1 = "52";
+    cout << "Input: " << num1 << ", Output: " << largestOddNumber(num1) << endl; // Ожидаемый вывод: "5"
+
+    string num2 = "4206";
+    cout << "Input: " << num2 << ", Output: " << largestOddNumber(num2) << endl; // Ожидаемый вывод: ""
+
+    string num3 = "35427";
+    cout << "Input: " << num3 << ", Output: " << largestOddNumber(num3) << endl; // Ожидаемый вывод: "35427"
+
+    return 0;
+}
